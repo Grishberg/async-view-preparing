ccc45429178da70f71c2e751e6331df58440a44b
Grigory Rylov <grishberg@gmail.com>
2019-10-23 08:42:24 +0300

    refactor idle task

--- lib-idle-handler/src/main/java/com/github/grishberg/idlehangler/IdleTaskManager.java
+++ lib-idle-handler/src/main/java/com/github/grishberg/idlehangler/IdleTaskManager.java
@@ -2,4 +2,5 @@ package com.github.grishberg.idlehangler;
 
 public interface IdleTaskManager {
     void addTask(IdleTask task);
+    void removeTask(IdleTask task);
 }
--- lib-idle-handler/src/main/java/com/github/grishberg/idlehangler/IdleTaskManagerImpl.java
+++ lib-idle-handler/src/main/java/com/github/grishberg/idlehangler/IdleTaskManagerImpl.java
@@ -1,54 +1,42 @@
 package com.github.grishberg.idlehangler;
 
-import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.os.MessageQueue;
 import android.support.annotation.MainThread;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.*;
+import java.util.LinkedList;
 
 @MainThread
 public class IdleTaskManagerImpl implements IdleTaskManager {
     private LinkedList<IdleTask> tasks = new LinkedList<>();
     private CustomIdleHandler idleHandler = new CustomIdleHandler();
-    private MessageQueue messageQueue;
-
-    public IdleTaskManagerImpl() {
-        messageQueue = Looper.getMainLooper().getQueue();
-    }
 
     @Override
     public void addTask(IdleTask task) {
         if (tasks.isEmpty()) {
-            messageQueue.addIdleHandler(idleHandler);
+            Looper.myQueue().addIdleHandler(idleHandler);
         }
-		tasks.offer(task);
+        tasks.offer(task);
     }
-	
-	public void removeTask(IdleTask task){
-		tasks.remove(task);
-	}
 
-    private void execute() {
-        if (tasks.isEmpty()) {
-            return;
-        }
-
-        Iterator<IdleTask> iterator = tasks.iterator();
-        while (iterator.hasNext()) {
-            IdleTask task = iterator.next();
-            task.run();
-            iterator.remove();
-        }
+    @Override
+    public void removeTask(IdleTask task) {
+        tasks.remove(task);
     }
 
     private class CustomIdleHandler implements MessageQueue.IdleHandler {
         @Override
         public boolean queueIdle() {
-            execute();
+            if (tasks.isEmpty()) {
+                return false;
+            }
+
+            for (IdleTask task : tasks) {
+                task.run();
+            }
+
+            tasks.clear();
+
             return false;
         }
     }
